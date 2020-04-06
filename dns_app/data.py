e447f1b9a44e896cd822056817253ec936387a88
Mr Grey <ksmaybe@gmail.com>
2020-04-05 21:07:22 -0400

    gogo

---
+++ dns_app/data.py
@@ -0,0 +1,15 @@
+import pickle
+
+#part1
+dic={}
+
+d={"ip":'value',"name":"name2","ttl":"10"}
+dic["name1"]=d
+print(dic["name1"])
+with open('my_dict.txt','wb') as f:
+    pickle.dump(dic,f)
+
+#part2
+with open('my_dict.txt','rb') as f:
+    dic=pickle.load(f)
+print(dic["name1"])
