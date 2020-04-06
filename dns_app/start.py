2556482fdf8d1e45bdae9da29585da5d2f5602a7
Mr Grey <ksmaybe@gmail.com>
2020-04-05 21:25:22 -0400

    gogo

--- dns_app/start.py
+++
@@ -1,8 +0,0 @@
-import requests
-data={
-    "hostname":"fibonacci.com",
-    "ip":"127.0.0.1",
-    "as_ip":"127.0.0.1",
-    "as_port":"53533"
-}
-print(requests.put("http://127.0.0.1:9090/register",json=data).content)
